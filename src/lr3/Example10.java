2cccae2d1b09dfe407ddd5ac099af26e2358ac9d
VershininDaniil <vershinin.danechka@mail.ru>
2023-12-12 23:00:21 +0500

    initial commit

--- src/lr3/Example10.java
+++ src/lr3/Example10.java
@@ -5,19 +5,19 @@ import java.util.Arrays;
 import java.util.Comparator;
 public class Example10 {
     public static void main(String[] args) {
-        Scanner id = new Scanner(System.in); // Создание объекта класса Scanner для считывания числа введённого к консоль
-        System.out.println("Введите размер массива"); // Сообщение пользователю "для красоты и понимания"
-        int size = id.nextInt(); // Запись в переменную Size (нужная для того чтобы задать размер массива)
-        //значения введенного в консоль
-        System.out.println("Размер массива равен "+ size); // Сообщение пользователю "для красоты и понимания"
-        int[] nums = new int[size]; // Создание массива с размером введённым из консоли
-        Random random = new Random(); // Создание объекта класса Random для генерации "случайного" числа
+        Scanner id = new Scanner(System.in);
+        System.out.println("Введите размер массива");
+        int size = id.nextInt();
+
+        System.out.println("Размер массива равен "+ size);
+        int[] nums = new int[size];
+        Random random = new Random();
         for (int i = 0 ; i < nums.length ; i++ ){
-            nums[i] = random.nextInt(200); // Присвоение i-тому элементу массива случайного значения
-            System.out.println("Элемент массива ["+i+"] = " + nums[i]); // Сообщение пользователю "для красоты и понимания"
+            nums[i] = random.nextInt(200);
+            System.out.println("Элемент массива ["+i+"] = " + nums[i]);
         }
-        Arrays.sort(nums); // Сортировка массива по возрастанию его элементов
-        System.out.println("Произведена сортировка массива"); // Сообщение пользователю "для красоты и понимания"
+        Arrays.sort(nums);
+        System.out.println("Произведена сортировка массива");
         for (int i = 0 ; i < nums.length ; i++ ) {}
         int[] nums2 = Arrays.stream(nums).boxed()
                 .sorted(Comparator.reverseOrder())
--- src/lr3/Example9.java
+++ src/lr3/Example9.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 import java.util.Random;
 public class Example9 {
     public static void main(String[] args) {
-        Scanner id = new Scanner(System.in); // Создание объекта класса Scanner для считывания числа введённого к консоль
-        System.out.println("Введите размер массива"); // Сообщение пользователю "для красоты и понимания"
-        int size = id.nextInt(); // Запись в переменную Size (нужная для того чтобы задать размер массива)
-        System.out.println("Размер массива равен " + size); // Сообщение пользователю "для красоты и понимания"
-        int[] nums = new int[size]; // Создание массива с размером введённым из консоли
-        Random random = new Random(); // Создание объекта класса Random для генерации "случайного" числа
+        Scanner id = new Scanner(System.in);
+        System.out.println("Введите размер массива");
+        int size = id.nextInt();
+        System.out.println("Размер массива равен " + size);
+        int[] nums = new int[size];
+        Random random = new Random();
         int min = 1000;
         int index = 0;
         int min2 = 1000;
